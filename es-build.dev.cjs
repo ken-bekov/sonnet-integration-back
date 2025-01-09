4e30ea22b8605fb72772bf1e0b03d345909a0915
Saken Sultanbekov <saken@xappex.com>
2025-01-09 12:41:02 +0500

    pass env vars in dev mode

--- es-build.dev.cjs
+++ es-build.dev.cjs
@@ -2,40 +2,48 @@ const esbuild = require('esbuild');
 const child_processes = require('child_process');
 
 (async () => {
-   const context = await esbuild.context({
-      entryPoints: ['./src/index.ts'],
-      outfile: './dist/ai-integration-backend.js',
-      bundle: true,
-      platform: 'node',
-      target: 'node18',
-      sourcemap: 'inline',
-      external: ['knex'],
-      plugins: [
-         {
-            name: 'rebuildPlugin',
-            setup: (build) => {
-               build.onEnd(result => {
-                  if (result.errors.length > 0) {
-                     result.errors.forEach(error => {
-                        console.error(error.text);
-                     })
-                  } else if (result.warnings.length > 0) {
-                     result.warnings.forEach(warning => {
-                        console.warn(warning.text);
-                     })
-                  } else {
-                     console.debug('Successfully rebuilt')
-                  }
-               });
+    const context = await esbuild.context({
+        entryPoints: ['./src/index.ts'],
+        outfile: './dist/ai-integration-backend.js',
+        bundle: true,
+        platform: 'node',
+        target: 'node18',
+        sourcemap: 'inline',
+        external: ['knex'],
+        plugins: [
+            {
+                name: 'rebuildPlugin',
+                setup: (build) => {
+                    build.onEnd(result => {
+                        if (result.errors.length > 0) {
+                            result.errors.forEach(error => {
+                                console.error(error.text);
+                            })
+                        } else if (result.warnings.length > 0) {
+                            result.warnings.forEach(warning => {
+                                console.warn(warning.text);
+                            })
+                        } else {
+                            console.debug('Successfully rebuilt')
+                        }
+                    });
+                }
             }
-         }
-      ]
-   });
-   await context.watch();
-   await child_processes
-       .spawn(
-           'node',
-           ['--watch', '--enable-source-maps', '--inspect=7070', './dist/ai-integration-backend.js'],
-           {env: {APP_PORT: 9090}, stdio: 'inherit'}
-       );
+        ]
+    });
+    await context.watch();
+    await child_processes
+        .spawn(
+            'node',
+            ['--watch', '--enable-source-maps', '--inspect=7070', './dist/ai-integration-backend.js'],
+            {
+                env: {
+                    APP_PORT: 9090,
+                    IDM_DB_USER: process.env.IDM_DB_USER,
+                    IDM_DB_PASSWORD: process.env.IDM_DB_PASSWORD,
+                    IDM_SONNET_API_KEY: process.env.IDM_SONNET_API_KEY,
+                },
+                stdio: 'inherit'
+            }
+        );
 })();
